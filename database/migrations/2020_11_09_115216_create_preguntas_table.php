c089b2fac87d812b89ffd5aa00a2b884d646acba
ninadgl <jannina.delosgodos@pucp.edu.pe>
2020-11-09 16:42:36 -0500

    table prg

--- database/migrations/2020_11_09_115216_create_preguntas_table.php
+++ database/migrations/2020_11_09_115216_create_preguntas_table.php
@@ -20,10 +20,10 @@ class CreatePreguntasTable extends Migration
             $table->decimal('puntaje',6,2);
             $table->integer('tipo');
             $table->integer('tipo_marcado');
+            $table->timestamp('fecha_creacion')->nullable();
             $table->timestamp('fecha_actualizacion')->nullable();
             $table->foreignId('tusuario_id_creacion')->nullable()->constrained('tUsuario');
             $table->foreignId('tusuario_id_actualizacion')->nullable()->constrained('tUsuario');
-            $table->timestamps();
         });
     }
 
